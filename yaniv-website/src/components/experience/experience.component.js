dc327e237b996fc52c6ffcdfd83865bcc2ab7b9d
Yaniv Suriyano <yanivsu@gmail.com>
2022-03-17 21:42:08 +0200

    no message

---
+++ yaniv-website/src/components/experience/experience.component.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+import { Card, Grid, makeStyles, Paper, Typography } from "@material-ui/core";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import CardContent from "@material-ui/core/CardContent";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+
+import * as fb from "../../config";
+import * as enums from "../../helprs/enums";
+import * as fbFuncs from "../../helprs/firebaseFunctions";
+import experienceAvatar from "../../styles/experienceAvatar.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#F0F0F5",
+  },
+  root2: {
+    marginTop: theme.spacing(25),
+  },
+  paper: {
+    padding: theme.spacing(10),
+    maxWidth: "350px",
+
+    backgroundColor: "#F0F0F5",
+  },
+  card: {
+    backgroundColor: "#F0F0F5",
+    width: "50%",
+    margin: theme.spacing(0, 0, 10, 6),
+    boxShadow: "3px 3px 3px 3px gray",
+  },
+
+  avatar: {
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+    color: "white ",
+    marginTop: theme.spacing(2),
+  },
+
+  timeline: {
+    color: "#2D2A46",
+  },
+}));
+
+function Experience() {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Grid
+        container
+        alignItems="center"
+        direction="column"
+        className={classes.root2}
+      >
+        <Typography variant="h2">{enums.EXPERIENCE}</Typography>
+        <img src={experienceAvatar} className={classes.avatar} />
+
+        <Card className={classes.card}>
+          <CardContent>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid item container>
+                <Timeline className={classes.timeline} align="left">
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot color="primary">
+                        <WorkIcon />
+                      </TimelineDot>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      <Paper elevation={2} className={classes.paper}>
+                        <Typography
+                          variant="h6"
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.SKYSOFT}
+                        </Typography>
+                        <Typography
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.SKYSOFT_ABOUT}
+                        </Typography>
+                        <Typography
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.SKYSOFT_YEARS}
+                        </Typography>
+                      </Paper>
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot color="primary">
+                        <WorkIcon />
+                      </TimelineDot>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      <Paper elevation={2} className={classes.paper}>
+                        <Typography
+                          variant="h6"
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.GENERIX}
+                        </Typography>
+                        <Typography
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.GENERIX_ABOUT}
+                        </Typography>
+                        <Typography
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.GENERIX_YEARS}
+                        </Typography>
+                      </Paper>
+                    </TimelineContent>
+                  </TimelineItem>
+                  <TimelineItem>
+                    <TimelineSeparator>
+                      <TimelineDot color="primary">
+                        <SchoolIcon />
+                      </TimelineDot>
+                      <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                      <Paper elevation={3} className={classes.paper}>
+                        <Typography
+                          variant="h6"
+                          component="h1"
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.EDUICATION}
+                        </Typography>
+                        <Typography
+                          style={{ color: "#2D2A46", textAlign: "start" }}
+                        >
+                          {enums.EDUICATION_ABOUT}
+                        </Typography>
+                      </Paper>
+                    </TimelineContent>
+                  </TimelineItem>
+                </Timeline>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Experience;
