f2f3f5523f803b52cc0c47723b1b2fa9c49c4338
Yaniv Suriyano <yanivsu@gmail.com>
2022-04-02 16:31:03 +0300

    Change name of project

--- yaniv-website/src/index.js
+++ yaniv-website/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+
+import { HashRouter, Route, Router, BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
+// ReactDOM.render(
+//   <HashRouter basename="/">
+//     <App />
+//   </HashRouter>,
+//   document.getElementById("root")
+// );
+
 ReactDOM.render(
-  <HashRouter basename="/">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
